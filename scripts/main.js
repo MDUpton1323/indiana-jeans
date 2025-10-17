3d6460f3bacc8da286bf587edbbd59f78be31212
Marcus Upton <MarcusDUpton13@gmail.com>
2025-10-16 23:11:26 -0500

    I have added radio buttons to the survey for locationChoices.js and have built html for that as well

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
-import { JeanChoices } from "./JeanChoices";
+import { JeanChoices } from "./JeanChoices.js";
+import { LocationChoices } from "./LocationChoices.js";
 
 const container = document.querySelector("#container");
 
-const render = () => {
+const render = async () => {
   const jeansHTML = JeanChoices();
+  const locationsHTML = await LocationChoices();
 
   container.innerHTML = `
     ${jeansHTML}
+    ${locationsHTML}
     `;
 };
-console.log(container);
+
 render();
